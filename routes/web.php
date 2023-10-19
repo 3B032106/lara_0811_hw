e2ad0cb39ae8c79338970fa07e4e889e2bda3db8
李宣佑 <3B032106@gm.student.ncut.edu.tw>
2023-10-19 18:49:54 +0800

    設定引入HomeController、AboutController、NewsController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
